525a5c743f8d9d0faf49a73d962eab34ea738f09
Parth Paradkar <parthparadkar3@gmail.com>
2019-10-30 19:56:15 +0530

    Author added to Team Card

--- src/views/Archive/MediumArticlesList.js
+++ src/views/Archive/MediumArticlesList.js
@@ -5,9 +5,10 @@ const MediumArticlesList = [
       'Until fairly recently, the dream of donning a headset and disappearing into a virtual world belonged purely to the realm of science fiction.',
     link:
       'https://medium.com/iit-technology-ambit/virtual-reality-lab-at-iit-kharagpur-b9c179736133',
-    date: 'Oct 10 2019',
+    date: 'Oct 10',
     min: '3',
-    img: 'vr.png'
+    img: 'vr.png',
+    author: "Koushiki Dasgupta"
   },
   {
     title: 'Novel 3D Printed Robotic Knee',
@@ -15,9 +16,10 @@ const MediumArticlesList = [
       'Modern prosthetic limbs are far more superior than those devloped in the last century but these new models have their limits.',
     link:
       'https://medium.com/iit-technology-ambit/novel-3d-printed-robotic-knee-6223a2a878d3',
-    date: 'Oct 9 2019',
+    date: 'Oct 9',
     min: '3',
-    img: '3d.jpeg'
+    img: '3d.jpeg',
+    author: "Jasmine Jerry A"
   },
   {
     title: 'And the Nobel for Physics goes to:',
@@ -25,18 +27,20 @@ const MediumArticlesList = [
       'This year’s Nobel Prize in Physics has been awarded jointly to James Peebles at Princeton University for “theoretical discoveries in physical cosmology”',
     link:
       'https://medium.com/iit-technology-ambit/and-the-nobel-for-physics-goes-to-f8a7e5252d17',
-    date: 'Oct 9 2019',
+    date: 'Oct 9',
     min: '3',
-    img: 'nobel.jpeg'
+    img: 'nobel.jpeg',
+    author: "Archi Banerjee"
   },
   {
     title: 'Heading down South : ISRO images the Lunar South Pole',
     desc: 'Exciting news coming this week from the South Pole of the moon!',
     link:
       'https://medium.com/iit-technology-ambit/heading-down-south-isro-images-the-lunar-south-pole-58a37c7e7445',
-    date: 'Oct 9 2019',
+    date: 'Oct 9',
     min: '3',
-    img: 'chand.png'
+    img: 'chand.png',
+    author: "Pranav Krishnan"
   },
   {
     title: 'Threads: Check out Instagram’s latest app!',
@@ -44,9 +48,10 @@ const MediumArticlesList = [
       'Facebook launched its newest app just 5 days back on October 3, 2019',
     link:
       'https://medium.com/iit-technology-ambit/threads-check-out-instagrams-latest-app-3ab866d722d4',
-    date: 'Oct 8 2019',
+    date: 'Oct 8',
     min: '3',
-    img: 'insta.jpeg'
+    img: 'insta.jpeg',
+    author: "Utkarsh Sinha"
   },
   {
     title: 'Look up! It’s a commercial drone!',
@@ -54,9 +59,10 @@ const MediumArticlesList = [
       'Jeff Bezos’ vision of drones delivering packages pronounced 6 years back is looking likely to find a place in our reality very soon.',
     link:
       'https://medium.com/iit-technology-ambit/look-up-its-a-commercial-drone-c5d4097cb5e2',
-    date: 'Oct 3 2019',
+    date: 'Oct 3',
     min: '3',
-    img: 'drone.jpeg'
+    img: 'drone.jpeg',
+    author: "Utkarsh Sinha"
   },
   {
     title: 'Indian Private Aerospace Sector lift-off: The countdown begins',
@@ -64,9 +70,10 @@ const MediumArticlesList = [
       'Aerospace start-ups have suddenly materialised in India with one clear aim- propel India as a global leader in the private space exploration industry.',
     link:
       'https://medium.com/iit-technology-ambit/indian-private-aerospace-sector-lift-off-the-countdown-begins-80829995391d',
-    date: 'Sep 27 2019',
+    date: 'Sep 27',
     min: '3',
-    img: 'rocket.png'
+    img: 'rocket.png',
+    author: "Shaurya Srivastava"
   },
   {
     title: 'AI is Predicting Solar Radiation',
@@ -74,9 +81,10 @@ const MediumArticlesList = [
       'Climate change has been a very pressing issue lately. All global leaders have been called to intervene, and alternative energy sources are a major go-to for most countries. ',
     link:
       'https://medium.com/iit-technology-ambit/ai-is-predicting-solar-radiation-1b8eb4dffdf',
-    date: 'Sep 26 2019',
+    date: 'Sep 26',
     min: '3',
-    img: 'rad.jpeg'
+    img: 'rad.jpeg',
+    author: "Jasmine Jerry A"
   },
   {
     title: 'Modernism vs. Post-Modernism',
@@ -84,9 +92,10 @@ const MediumArticlesList = [
       'Post Modernism is a bit difficult. But simplifying and trying to make it a lighter read would just take away the beauty of the topic. So gear up with a nice dictionary and focus attention.',
     link:
       'https://medium.com/iit-technology-ambit/modernism-vs-post-modernism-7f571c975c9b',
-    date: 'Sep 26 2019',
+    date: 'Sep 26',
     min: '3',
-    img: 'monroe.png'
+    img: 'monroe.png',
+    author: "Arya S Jyothi"
   },
   {
     title: 'Breaking down Hate Speech',
@@ -94,9 +103,10 @@ const MediumArticlesList = [
       'Social media has percolated through our lives so much that it has become an everyday conduit of social interaction. ',
     link:
       'https://medium.com/iit-technology-ambit/breaking-down-hate-speech-f4d1391940eb',
-    date: 'Sep 26 2019',
+    date: 'Sep 26',
     min: '3',
-    img: 'hate.jpeg'
+    img: 'hate.jpeg',
+    author: "Shikhar Mohan"
   }
 ];
 
