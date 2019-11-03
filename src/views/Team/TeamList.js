ffbe290ccad6267b9fd51b2cfe52b91f0802b928
Shivam Kumar Jha <shivam.cs.iit.kgp@gmail.com>
2019-11-04 00:46:34 +0530

    update contact page

--- src/views/Team/TeamList.js
+++ src/views/Team/TeamList.js
@@ -12,23 +12,23 @@ const TeamList = [
   {
     img: images['Utkarsh.jpg'],
     name: 'Utkarsh Sinha',
-    role: 'Founder',
+    role: 'Founder & CEO',
     fb: 'https://www.facebook.com/utkarsh.sinha.186',
     ln: 'https://www.linkedin.com/in/utksn15/'
   },
   {
-    img: images['Shivam.jpg'],
-    name: 'Shivam Kumar Jha',
-    role: 'Head of Technology',
-    fb: 'https://www.facebook.com/thealphadollar',
-    ln: 'https://www.linkedin.com/in/thealphadollar/'
-  },
-  {
     img: images['Suraj.jpg'],
     name: 'Suraj Iyyengar',
-    role: 'Editorial Head',
+    role: 'Chief Editor',
     fb: 'https://www.facebook.com/surajiyyengar',
     ln: 'https://www.linkedin.com/in/surajiyyengar/'
+  },
+  {
+    img: images['Shivam.jpg'],
+    name: 'Shivam Kumar Jha',
+    role: 'Head of Tech',
+    fb: 'https://www.facebook.com/thealphadollar',
+    ln: 'https://www.linkedin.com/in/thealphadollar/'
   },
   {
     img: images['Kartik.jpg'],
@@ -40,134 +40,126 @@ const TeamList = [
   {
     img: images['Unnikrishnan.jpg'],
     name: 'Unnikrishnan Nambiar',
-    role: 'Finance Team',
+    role: 'Head of Finance',
     fb: 'https://www.facebook.com/unnikrishnan.nambiar.sj',
     ln: 'https://www.linkedin.com/in/unnikrishnannambiark/'
   },
   {
     img: images['Shail.jpg'],
     name: 'Daswani Shail Manoj',
-    role: 'Editorial Team',
+    role: 'Senior Editor',
     fb: 'https://www.facebook.com/shail.daswani',
     ln: 'https://www.linkedin.com/in/shaildaswani/'
   },
   {
+    img: images['Aritra.jpg'],
+    name: 'Aritra Biswas',
+    role: 'Senior Editor',
+    fb: 'https://www.facebook.com/aritra.biswas.186',
+    ln: '#'
+  },
+  {
     img: images['Pranav.jpg'],
     name: 'Pranav Krishnan',
-    role: 'Editorial Team',
+    role: 'Editor',
     fb: 'https://www.facebook.com/pranavwashere',
     ln: 'https://www.linkedin.com/in/pranav-krishnan-97080a160/'
   },
   {
     img: images['Aditya.jpg'],
     name: 'Aditya Gandhi',
-    role: 'Editorial Team',
-    fb: '#',
+    role: 'Editor',
+    fb: 'https://m.facebook.com/adi.gandhi1',
     ln: 'https://www.linkedin.com/in/aditya-gandhi011/'
   },
   {
     img: images['Koushiki.jpg'],
     name: 'Koushiki Dasgupta',
-    role: 'Editorial Team',
+    role: 'Editor',
     fb: 'https://www.facebook.com/koushiki.dasguptachaudhuri.9',
     ln: 'https://www.linkedin.com/in/koushikidasguptachaudhuri99/'
   },
   {
     img: images['Archi.jpg'],
     name: 'Archi Banerjee',
-    role: 'Editorial Team',
+    role: 'Editor',
     fb: 'https://www.facebook.com/profile.php?id=100013952071030',
     ln: 'https://www.linkedin.com/in/archi-banerjee-a9b253196'
   },
   {
     img: images['Shikhar.jpg'],
     name: 'Shikhar Mohan',
-    role: 'Editorial Team',
+    role: 'Editor',
     fb: 'https://www.facebook.com/shikhar.mohan2',
     ln: 'https://www.linkedin.com/in/shikhar-mohan-a38606171'
   },
   {
     img: images['Shaurya.jpg'],
     name: 'Shaurya Shrivastava',
-    role: 'Editorial Team',
+    role: 'Editor',
     fb: 'https://www.facebook.com/shaurya.shrivastava23',
     ln: 'https://www.linkedin.com/in/shaurya-shrivastava-540677164/'
   },
   {
-    img: images['Aritra.jpg'],
-    name: 'Aritra Biswas',
-    role: 'Editorial Team',
-    fb: 'https://www.facebook.com/aritra.biswas.186',
-    ln: '#'
-  },
-  {
     img: images['Jasmine.jpg'],
     name: 'Jasmine Jerry A',
-    role: 'Editorial Team',
+    role: 'Editor',
     fb: 'https://www.facebook.com/jasmine.jerry.a',
     ln: 'https://www.linkedin.com/in/jasminejerrya/'
-  },
-
-  {
-    img: images['Aastha.jpg'],
-    name: 'Aastha Jakher',
-    role: 'Editorial Team',
-    fb: '#',
-    ln: '#'
   },
   {
     img: images['Shubham.jpg'],
     name: 'Shubham Mishra',
-    role: 'Technical Team',
+    role: 'Developer',
     fb: 'https://www.facebook.com/grapheo12',
     ln: 'https://www.linkedin.com/in/shubham-mishra-195ab1171/'
   },
   {
     img: images['Mukul.jpg'],
     name: 'Mukul Mehta',
-    role: 'Technical Team',
+    role: 'Developer',
     fb: 'https://www.facebook.com/mukul.amehta',
     ln: 'https://www.linkedin.com/in/mukul-mehta12/'
   },
   {
     img: images['Ajay.jpg'],
     name: 'Ajay Yadav',
-    role: 'Technical Team',
+    role: 'Developer',
     fb: 'https://www.facebook.com/ajajaj2807',
     ln: 'https://www.linkedin.com/in/ajajaj2807/'
   },
   {
     img: images['Raghavendra.jpg'],
     name: 'Raghavendra kaushik',
-    role: 'Technical Team',
+    role: 'Developer',
     fb: 'https://www.facebook.com/raghavendra.kaushik.3',
     ln: 'https://www.linkedin.com/in/raghavendra-kaushik13/'
   },
   {
     img: images['Parth.jpg'],
     name: 'Parth Paradkar',
-    role: 'Technical Team',
+    role: 'Developer',
     fb: 'https://www.facebook.com/parth.paradkar.35',
     ln: 'https://www.linkedin.com/in/parth-paradkar/'
   },
   {
     img: images['Tanishq.jpg'],
     name: 'Tanishq Kishnani',
-    role: 'Product Team',
+    role: 'Product Manager',
     fb: 'https://www.facebook.com/tanishq.kishnanimaux',
     ln: 'https://www.linkedin.com/in/tanishq-kishnani-4b0891194/'
   },
   {
     img: images['Mansi.jpg'],
     name: 'Mansi Shivhare',
-    role: 'Product Team',
+    role: 'Product Designer',
     fb: 'https://www.facebook.com/mansi.shivhare.94',
     ln: 'https://www.linkedin.com/in/mansi-shivhare-4b79b4173/'
   },
   {
     img: images['Haveen.jpg'],
     name: 'Haveen Hrithic TL',
-    role: 'Product Team',
+    role: 'Product Designer',
     fb: 'https://www.facebook.com/haveen.hrithictl',
     ln: 'https://www.linkedin.com/in/haveen-hrithic-90916716b'
   }
