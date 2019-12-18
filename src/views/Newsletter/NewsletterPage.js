005e71bf36c66cae53a66180308bb43de86be97b
rakaar <rka87338@gmail.com>
2019-12-18 20:30:27 +0530

    Newsletter from sendgrid is embeded successfully

--- src/views/Newsletter/NewsletterPage.js
+++ src/views/Newsletter/NewsletterPage.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import SocialMediaButtons from './SocialMediaButtons';
 import SubscribeInput from './SubscribeInput';
-
+import renderHTML from 'react-render-html';
+import html from './news-letter'
 import './NewsletterPage.scss'
 
 export default function Newsletter() {
     return(
         <div>
             <div className='newsletter'>
-                <h1>Newsletter</h1>
+              {renderHTML(html)}
             </div>
             <SocialMediaButtons />
             <SubscribeInput />
