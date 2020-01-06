19391eaa023e909393251dca697850f533e8839f
rakaar <rka87338@gmail.com>
2020-01-07 00:57:58 +0530

    Stuck at accessing style of the element obtained

--- src/views/Newsletter/NewsletterPage.js
+++ src/views/Newsletter/NewsletterPage.js
@@ -18,6 +18,16 @@ export default function Newsletter() {
     const [html, SetHtml] = useState("<div style='margin-top: 30vh;margin-left: 15vh;'><img style='height:16vh;' src='" + LoaderSVG + "' /></div>");
 
     useEffect(() => {
+       var x = document.getElementsByTagName('center')
+    var y = document.getElementsByClassName('wrapper')
+        // THIS PRITNS SUCCESSFULLY 
+        console.log('center_html is ', x);
+        // this is undefined 
+        // as per this 
+        console.log('why th efuck ', Array.from(x))
+        // these are also undefined
+        console.log('why the hell ', x[0], [...x])
+
         const fetchData = async () => {
           let res = await integrate.getData(newsletterEndpoint, {});
           if (res.data.resource.newsletter_content == undefined) window.location.reload();
