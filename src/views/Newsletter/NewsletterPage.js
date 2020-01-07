d2bfec6f8ce24ee95675666dc3b2b196eaa0548f
rakaar <rka87338@gmail.com>
2020-01-07 14:20:32 +0530

    Changed error handling in newsletter

--- src/views/Newsletter/NewsletterPage.js
+++ src/views/Newsletter/NewsletterPage.js
@@ -20,7 +20,7 @@ export default function Newsletter() {
     useEffect(() => {
         const fetchData = async () => {
           let res = await integrate.getData(newsletterEndpoint, {});
-          if (res.data.resource.newsletter_content == undefined) window.location.reload();
+          if (res.data == undefined) window.location.reload();
           SetHtml(res.data.resource.newsletter_content);
         };
         fetchData();
