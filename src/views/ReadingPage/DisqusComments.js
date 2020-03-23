4c39e1eca4e74b65f73c48886578adf6b747852d
rashil2000 <rashil2000@gmail.com>
2020-03-23 18:47:34 +0530

    Added Disqus comments feature

---
+++ src/views/ReadingPage/DisqusComments.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { DiscussionEmbed } from 'disqus-react';
+
+class Comments extends React.Component {
+  render() {
+    const disqusShortname = "iit-techambit-in";
+    const id = window.location.pathname.split('/')[2];
+    const disqusConfig = {
+      identifier: id
+    };
+    console.log(window.location.pathname);
+    return (
+      <div className="comment">
+        <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
+      </div>
+    );
+  }
+}
+
+export default Comments;
